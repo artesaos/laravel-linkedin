25a392a70dc86954d159fcdc32a09a97c6d722db
Mauri de Souza Nunes <mauri870@gmail.com>
2015-12-11 14:02:02 -0200

    Create facade

---
+++ src/Mauri870/LinkedIn/Facades/LinkedIn.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Linkedin API for Laravel Framework
+ *
+ * @author    Mauri de Souza Nunes <mauri870@gmail.com>
+ * @copyright Copyright (c) 2015, Mauri de Souza Nunes <github.com/mauri870>
+ * @license   https://opensource.org/licenses/MIT MIT License
+ */
+
+namespace Mauri870\LinkedIn\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class LinkedIn extends Facade {
+
+    protected static function getFacadeAccessor() {
+        return 'LinkedIn';
+    }
+}
