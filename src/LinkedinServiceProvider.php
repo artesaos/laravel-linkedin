5199aa2f41e84334f271b72b17081b4e9d097d0a
Mauri de Souza Nunes <mauri870@gmail.com>
2015-12-11 11:05:16 -0200

    Initial commit

---
+++ src/LinkedinServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Linkedin API for Laravel Framework
+ *
+ * @author    Mauri de Souza Nunes <mauri870@gmail.com>
+ * @copyright Copyright (c) 2015, Mauri de Souza Nunes <github.com/mauri870>
+ * @license   https://opensource.org/licenses/MIT MIT License
+ */
+
+namespace Mauri870\Linkedin;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class LinkedinServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
